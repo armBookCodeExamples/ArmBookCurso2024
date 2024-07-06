eb8fcbf5f73644c31fa30615f222b2f54553be2e
manucho1586 <ing.emanuelgarcia95@gmail.com>
2024-07-06 22:28:06 +0000

    probar

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include "mbed.h"
+#include "DHT.h"
+#include "arm_book_lib.h"
 
 int main()
 {
+    DHT dht(D8, DHT::DHT11);
     DigitalIn B1(BUTTON1);
 
     DigitalOut Teyu(LED1);
 
-    while (true) {
+    while (true)
+    {
         Teyu = !B1;
+        delay(3);
+        int err = dht.read();
+        if (err == DHT::SUCCESS)
+        {
+            printf("T = %.1f°C\r\n", dht.getTemperature());
+        } 
+        else
+        {
+            printf("Error code : %d\r\n", err);
+        }
+        delay(3);
     }
 }
